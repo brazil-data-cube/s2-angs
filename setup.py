645d426afafe5354efd05a899026bb920baa638c
marujore <rennanmarujo@gmail.com>
2022-09-21 13:47:28 -0300

    Review License - GPL v3

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 #
-# This file is part of Python Client Library for Sentinel-2 Angle Bands.
-# Copyright (C) 2021 INPE.
+# This file is part of Brazil Data Cube Sentinel-2 Angle Bands.
+# Copyright (C) 2022 INPE.
 #
-# Python Client Library for Sentinel-2 Angle Bands is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
 #
 
 """Python Client Library for Sentinel-2 Angle Bands."""
@@ -70,7 +80,7 @@ setup(
     long_description=readme + '\n\n' + history,
     long_description_content_type = 'text/x-rst',
     keywords=['Time series', 'Earth Observations'],
-    license='MIT',
+    license='GPLv3',
     author='Brazil Data Cube Team',
     author_email='brazildatacube@inpe.br',
     url='https://github.com/brazil-data-cube/s2-angs',
@@ -94,7 +104,7 @@ setup(
         'Environment :: Web Environment',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
